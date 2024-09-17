e93621cd8e31dc3aa8954511df5a8423ac84693c
moses-ramoeletsi <mramoeletsi14@gmail.com>
2024-09-17 08:53:57 +0200

    added firebase config fiile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCBrTHO2Bf2SX3TuPcq5LWV5A56rbpPDY",
+    authDomain: "livestockcare-7087d.firebaseapp.com",
+    projectId: "livestockcare-7087d",
+    storageBucket: "livestockcare-7087d.appspot.com",
+    messagingSenderId: "617203668537",
+    appId: "1:617203668537:web:f791142e57fe156eaa5fe8"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyALXXLAuA8oaIP786ZTrV6DGm8vaYztVYU",
+    authDomain: "mediconnect-66bab.firebaseapp.com",
+    projectId: "mediconnect-66bab",
+    storageBucket: "mediconnect-66bab.appspot.com",
+    messagingSenderId: "259864051421",
+    appId: "1:259864051421:web:aec1fb114101d0f763a534"
+  }
 };
 
 /*
