112c00be210b42d41f29d3068813d8bed90adbac
moses-ramoeletsi <mramoeletsi14@gmail.com>
2024-09-17 13:24:05 +0200

    made modifications

--- src/app/pages/Patients/upcoming-events/upcoming-events.page.ts
+++ src/app/pages/Patients/upcoming-events/upcoming-events.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventsService } from 'src/app/services/events.service';
 
 @Component({
   selector: 'app-upcoming-events',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upcoming-events.page.scss'],
 })
 export class UpcomingEventsPage implements OnInit {
-
-  constructor() { }
+  event= {
+    event_title: '',
+    description: '',
+    date_and_time: '',
+    location: '',
+    host: '',
+  };
+  events: any[] = [];
+  constructor( public fireStore: EventsService) { }
 
   ngOnInit() {
+    this.fetchPostedEvents();
   }
-
+  
+  fetchPostedEvents() {
+    this.fireStore.fetchPostedEvents().subscribe((events) => {
+      this.events = events;
+    });
+  }
+  getFormattedDateTime(dateTimeStr: string): { date: string; time: string } {
+    const dateTime = new Date(dateTimeStr);
+    const year = dateTime.getFullYear();
+    const month = dateTime.getMonth() + 1; 
+    const date = dateTime.getDate();
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes();
+  
+    const formattedDate = `${year}-${month}-${date}`;
+    const formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+  
+    return { date: formattedDate, time: formattedTime };
+  }
 }
