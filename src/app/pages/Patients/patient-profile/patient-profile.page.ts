e47181cd8bd6eeeeceeb13de71505441f75346bb
moses-ramoeletsi <mramoeletsi14@gmail.com>
2024-09-17 10:56:13 +0200

    added the user profile

--- src/app/pages/Patients/patient-profile/patient-profile.page.ts
+++ src/app/pages/Patients/patient-profile/patient-profile.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-patient-profile',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientProfilePage implements OnInit {
 
-  constructor() { }
+  currentUser: any;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private alertController: AlertController,
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.userService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        this.currentUser = user;
+      }
+    });
+  }
+
+  logout() {
+    this.presentLogoutAlert();
+  }
+
+  async presentLogoutAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm Logout',
+      message: 'Are you sure you want to logout?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+          }
+        }, {
+          text: 'Logout',
+          handler: () => {
+            this.afAuth.signOut().then(() => {
+              localStorage.clear();
+              sessionStorage.clear();
+              this.router.navigate(['/login']);
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
